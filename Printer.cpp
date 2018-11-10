7a2e5a518255752a3e43e9e9925c526220cb3bc5
YazHek <kruchookjarema@gmail.com>
2018-11-11 00:16:08 +0200

    Added implementation of get methods to variables of header file.

---
+++ Printer.cpp
@@ -0,0 +1,29 @@
+
+#include "Printer.h"
+
+
+
+string printer::getName() {
+    
+    return name;
+}
+
+int printer::getSpeed(){
+    
+    return speed;
+}
+
+int printer::getPrice() {
+    
+    return price;
+}
+
+int printer::getType(){
+    
+    return type;
+}
+
+int printer::getDruk(){
+    
+    return druk;
+}
